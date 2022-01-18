568d189e454a7ff6ea54f59a9d04170d7adea830
Arsalan <airavani2018@ada.edu.az>
2022-01-18 21:22:29 +0100

    flickering reduced

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,6 @@
 #define COLOR_ORDER BRG
 #define NUM_LEDS 66
 
-#define LOG_D(fmt, ...) printf_P(PSTR(fmt "\n"), ##__VA_ARGS__);
-
 CRGB leds[NUM_LEDS];
 
 // access your HomeKit characteristics defined in my_accessory.c
@@ -20,15 +18,13 @@ extern "C" homekit_server_config_t accessory_config;
 extern "C" homekit_characteristic_t cha_on;
 extern "C" homekit_characteristic_t cha_bright;
 
-static uint32_t next_heap_millis = 0;
-
 void setBrightness(int newBrightness) {
-  int delayTime = 20;
+  // int delayTime = 15;
 
   Serial.println(newBrightness);
 
   FastLED.setBrightness(map(newBrightness, 0, 100, 0, 255));
-  FastLED.delay(delayTime);
+  // FastLED.delay(delayTime);
 }
 
 void smoothBrightness(int from, int to) {
@@ -64,26 +60,15 @@ void set_bright(const homekit_value_t v) {
 }
 
 void my_homekit_setup() {
-
   cha_on.setter = set_on;
   cha_bright.setter = set_bright;
-
   arduino_homekit_setup(&accessory_config);
 }
 
-void my_homekit_loop() {
-  arduino_homekit_loop();
-  const uint32_t t = millis();
-  if (t > next_heap_millis) {
-    // show heap info every 5 seconds
-    next_heap_millis = t + 5 * 1000;
-    LOG_D("Free heap: %d, HomeKit clients: %d", ESP.getFreeHeap(),
-          arduino_homekit_connected_clients_count());
-  }
-}
+void my_homekit_loop() { arduino_homekit_loop(); }
 
 void setup() {
-  delay(2000); // for recovery
+  delay(4000); // for recovery
 
   Serial.begin(9600);
 
@@ -91,11 +76,11 @@ void setup() {
 
   my_homekit_setup();
 
-  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS)
-      .setCorrection(TypicalLEDStrip)
-      // .setDither();
+  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  // .setCorrection(TypicalLEDStrip);
+  // .setDither();
 
-      FastLED.setBrightness(cha_bright.value.int_value);
+  FastLED.setBrightness(cha_bright.value.int_value);
 }
 
 // This function draws rainbows with an ever-changing,
@@ -143,11 +128,7 @@ void rainbow() {
 void loop() {
   rainbow();
 
-  my_homekit_loop();
-
-  delay(10);
-
   FastLED.show();
 
-  FastLED.delay(10);
+  my_homekit_loop();
 }
